cab740724295431f48c8f2fedeacbb39fb94bf4e
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-11-01 10:01:15 +0200

    Project start

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log('Your app is listening on port ' + listener.address().port)
+  })
