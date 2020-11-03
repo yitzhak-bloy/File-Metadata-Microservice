b6a037ef7ebaae1dad923ef1b34dde6a420e18c5
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-11-03 10:26:59 +0200

    I will receive the file name, type, and size in bytes within the JSON response

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 // require and use "multer"...
 
@@ -13,6 +15,16 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({
+    name: req.file.originalname, 
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
